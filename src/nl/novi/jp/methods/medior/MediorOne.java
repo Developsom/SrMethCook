b30be4f2e9a304d00dbb8768a7040702d1acfecf
Developsom <100343391+Developsom@users.noreply.github.com>
2023-06-24 21:09:37 +0200

    Fix readability

---
+++ src/nl/novi/jp/methods/medior/MediorOne.java
@@ -0,0 +1,30 @@
+package nl.novi.jp.methods.medior;
+
+/**
+ * Kun je een methode schrijven die het getal kwadrateert en deze returnt als integer?
+ *
+ * Roep deze vervolgens aan vanuit de main-methode.
+ */
+
+public class MediorOne {
+
+    public static void main(String[] args) {
+        int result = calculateSum(10, 22);
+
+        System.out.println("De som van 10 en 12 is: " + result);
+
+        int doubledResult = doubleNumber(result);
+
+        System.out.println(result + " verdubbeld is: " + doubledResult);
+    }
+
+    public static int calculateSum(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    public static int doubleNumber(int number) {
+        return number * 2;
+    }
+
+
+}
