a8516c6096bba6b3b7dda934309f594ab4508504
Developsom <100343391+Developsom@users.noreply.github.com>
2023-06-24 14:21:59 +0200

    Assignment code

---
+++ src/nl/novi/jp/methods/senior/SrMethCook.java
@@ -0,0 +1,23 @@
+package nl.novi.jp.methods.senior;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SrMethCook {
+    public static void main(String[] args) {
+        List<String> curstomerNames = new ArrayList<>();
+
+        curstomerNames.add("nick piraat");
+        curstomerNames.add("michael jackson");
+        curstomerNames.add("glennis grace");
+        curstomerNames.add("dreetje hazes");
+        curstomerNames.add("robbie williams");
+        curstomerNames.add("michiel de ruyter");
+        curstomerNames.add("sjaak polak");
+        curstomerNames.add("jan van jansen");
+        curstomerNames.add("henk den hartog");
+        curstomerNames.add("mo el-mecky");
+        curstomerNames.add("fredje kadetje");
+    }
+
+}
