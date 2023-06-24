aa4f5ad1f121dbd049aabef99c4d770e904c714e
Developsom <100343391+Developsom@users.noreply.github.com>
2023-06-24 14:25:47 +0200

    Moved back: Assignment description

--- src/nl/novi/jp/methods/senior/SrMethCook.java
+++ src/nl/novi/jp/methods/senior/SrMethCook.java
@@ -3,6 +3,18 @@ package nl.novi.jp.methods.senior;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Gegeven: een lijst met namen zonder hoofdletters van klanten.
+ * Opdracht: Een luie stagiair heeft alle klanten in het systeem gezet, maar deze kon de Shift-knop niet vinden.
+ * Het is aan jou om alle namen van een hoofdletter aan het begin te voorzien. Maak een methode die een List als
+ * parameter ontvangt en de inhoud vervolgens van hoofdletters voorziet.
+ *
+ * Bonus 1: Zorg ervoor dat tussenvoegsels geen hoofdletter krijgen.
+ * Bonus 2: Zorg ervoor dat de eerste letter na een '-' wel een hoofdletter krijgt.
+ *
+ *
+ */
+
 public class SrMethCook {
     public static void main(String[] args) {
         List<String> customerNames = new ArrayList<>();
