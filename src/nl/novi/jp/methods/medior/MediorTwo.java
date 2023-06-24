e8b30e9fe02ee9cf44ac40daba722cc83291b1d8
Developsom <100343391+Developsom@users.noreply.github.com>
2023-06-24 22:23:09 +0200

    Fixed readability

---
+++ src/nl/novi/jp/methods/medior/MediorTwo.java
@@ -0,0 +1,32 @@
+package nl.novi.jp.methods.medior;
+
+/**
+ * Gegeven:Methode die boolean teruggeeft op basis van de gegeven leeftijd.
+ *
+ * Roep isOldEnough vanuit de main-methode aan.
+ * Op basis van de return value:
+ * Roep je of printUnderAgeWarning() of printProceed() uit.
+ */
+public class MediorTwo {
+    int
+
+    public static void main(String[] args) {
+
+    }
+
+    public  static boolean isOldEnough(int age) {
+        if(age > 17) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void printUnderAgeWarning() {
+        System.out.println("You are not old enough for this activity!");
+    }
+
+    public static void printProceed() {
+        System.out.println("No warning!");
+    }
+}
