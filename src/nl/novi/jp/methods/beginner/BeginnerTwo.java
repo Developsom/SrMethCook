abcf948e0905ccb1c55b1b3223685464aa5c9340
Developsom <100343391+Developsom@users.noreply.github.com>
2023-06-24 16:45:26 +0200

    Moved descrp

--- src/nl/novi/jp/methods/beginner/BeginnerTwo.java
+++ src/nl/novi/jp/methods/beginner/BeginnerTwo.java
@@ -1,14 +1,5 @@
 package nl.novi.jp.methods.beginner;
 
-/**
- * Een ander woord voor een methode uitvoeren is aanroepen. Je kunt in Java dus een methode schrijven zonder deze uit
- * te voeren. Dat is in deze opdracht ook gebeurd.
- *
- * De methode printFact() wordt al uitgevoerd. Zorg ervoor dat printAnotherFact() ook wordt uitgevoerd.
- *
- * Je kunt in BeginnerOne kijken hoe twee methodes na elkaar worden aangeroepen. Wat zie je in IntelliJ veranderen aan
- * de kleur van printAnotherFact()?
- */
 public class BeginnerTwo {
     public static void main(String[] args) {
         printFact();
@@ -22,3 +13,14 @@ public class BeginnerTwo {
         System.out.println("Je kunt System.out.println versneld typen door 'sout' + 'Enter' te typen");
     }
 }
+
+
+/**
+ * Een ander woord voor een methode uitvoeren is aanroepen. Je kunt in Java dus een methode schrijven zonder deze uit
+ * te voeren. Dat is in deze opdracht ook gebeurd.
+ *
+ * De methode printFact() wordt al uitgevoerd. Zorg ervoor dat printAnotherFact() ook wordt uitgevoerd.
+ *
+ * Je kunt in BeginnerOne kijken hoe twee methodes na elkaar worden aangeroepen. Wat zie je in IntelliJ veranderen aan
+ * de kleur van printAnotherFact()?
+ */
